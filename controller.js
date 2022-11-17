9ca5956ea1e5746bd35bc350868a30cf9f34d029
Eric Kouassi <39168930+erickouassi@users.noreply.github.com>
2022-11-16 22:22:55 -0500

    Add file

---
+++ controller.js
@@ -0,0 +1,214 @@
+// Logic behind the functionalities
+var serverTime = "America/New_York";  // America/New_York /
+
+// current datetime string inAmerica/New_York timezone
+let local_datetime_str = new Date().toLocaleString("en-US", { timeZone: serverTime });
+
+// create new Date object
+//let date_local = new Date(local_datetime_str);
+let d = new Date(local_datetime_str);
+
+// Months
+let allMonths = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+let monthTxt = allMonths[d.getMonth()];  // May
+
+// year as (YYYY) format
+let year = d.getFullYear();
+
+// month as (MM) format
+let month = ("0" + (d.getMonth() + 1)).slice(-2);
+
+// date as (DD) format
+let date = ("0" + d.getDate()).slice(-2);
+
+// date time in YYYY-MM-DD format
+//let date_time = year + "-" + month + "-" + date;
+let today = month + "/" + date + "/" + year; // "11/13/2022"
+//console.log(today);
+
+
+
+//
+// Yesterday
+const y = new Date(d);
+y.setDate(y.getDate() - 1);
+let y_dd = String(y.getDate()).padStart(1, '0'); // 17
+//let y_day = weekday[y.getDay()];   // Tuesday
+//let y_month = allMonths[y.getMonth()];  // May
+let y_mm = String(y.getMonth() + 1).padStart(1, '0'); // 11
+let y_yyyy = y.getFullYear();  // 2022
+
+let yesterday = y_mm +'/'+ y_dd +'/'+ y_yyyy;   //  -> 11/12/2022
+//console.log(yesterday);
+
+
+// Tomorrow
+const t = new Date(d);
+t.setDate(t.getDate() + 1);
+let t_dd = String(t.getDate()).padStart(1, '0'); // 19
+//let t_day = weekday[t.getDay()];   // Thursday
+//let t_month = allMonths[t.getMonth()];  // May
+let t_mm = String(t.getMonth() + 1).padStart(1, '0'); // 11
+let t_yyyy = t.getFullYear();  // 2022
+
+let tomorrow = t_mm +'/'+ t_dd +'/'+ t_yyyy;   //  -> 11/14/2022
+//console.log(tomorrow);
+
+// Next 7 days start & end
+const n = new Date(d);
+n.setDate(n.getDate() + 7);
+let n_dd = String(n.getDate()).padStart(1, '0'); // 19
+//let n_day = weekday[n.getDay()];   // Thursday
+//let n_month = allMonths[n.getMonth()];  // May
+let n_mm = String(t.getMonth() + 1).padStart(1, '0'); // 11
+let n_yyyy = n.getFullYear();  // 2022
+
+let end_day = n_mm +'/'+ n_dd +'/'+ n_yyyy;   //  -> May 19, 2022
+//console.log(end_day);
+
+//const data = require("./data");
+const data = require("./data_asp_nyc");
+
+
+const index_start = data.map(i => i.fullDate).indexOf(today);
+//console.log(index);
+const index_end = data.map(i => i.fullDate).indexOf(end_day);
+//const id_num = index_end + 1;
+//console.log(id_num);
+
+const start_index = data.map(i => i.dateNum).indexOf("01/01");
+//console.log(start_index);
+const end_index = data.map(i => i.dateNum).indexOf("12/31");
+//console.log(end_index);
+
+
+class Controller {
+  // getting all data
+  async getAllData() {
+    // return all data
+    return new Promise((resolve, _) => resolve(data));
+  }
+//
+  // getting a single data
+  async getSingleData(X) {
+    return new Promise((resolve, reject) => {
+      // get the data
+      let singleData = data.find((Y) => Y.dateMMDDYY === X);
+
+      if (singleData) {
+        // return the data
+        resolve(singleData);
+      } else {
+        // return an error
+        reject(`Object with id ${X} not found `);
+      }
+    });
+  }
+//
+ // getting the next 7 days data
+ async get7Data() {
+  return new Promise((resolve, reject) => {
+    // get the data
+   // slice from 1..3 - add 1 as the end index is not included
+const data_7_days = data.slice(index_start, index_end);
+
+    if (data_7_days) {
+      // return the data
+      resolve(data_7_days);
+    } else {
+      // return an error
+      reject(`Object 7 days data  not found `);
+    }
+  });
+}
+//
+// getting full year data
+async getYearData() {
+    return new Promise((resolve, reject) => {
+      // get the data
+     // slice from 1..3 - add 1 as the end index is not included
+  const year_data = data.slice(start_index, end_index + 1);
+  
+      if (year_data) {
+        // return the data
+        resolve(year_data);
+      } else {
+        // return an error
+        reject(`Object Year data  not found `);
+      }
+    });
+  }
+  //
+  // getting today data
+  async getTodayData() {
+    return new Promise((resolve, reject) => {
+      // get the data
+	  let todayData = data.filter(function(todayIn) {
+    return todayIn.fullDate == today; });
+   // console.log(todayData);
+//
+      if (todayData) {
+        // return the data
+        resolve(todayData);
+      } else {
+        // return an error
+        reject(`Today object not found `);
+      }
+    });
+  }
+  
+  // getting yesterday data
+  async getYesterdayData() {
+    return new Promise((resolve, reject) => {
+      // get the data
+	  let yesterdayData = data.filter(function(yesterdayIn) {
+    return yesterdayIn.fullDate == yesterday; });
+   // console.log(yesterdayData);
+      if (yesterdayData) {
+        // return the data
+        resolve(yesterdayData);
+      } else {
+        // return an error
+        reject(`Yesterday object not found `);
+      }
+    });
+  }
+  
+   // getting tomorrow data
+   async getTomorrowData() {
+    return new Promise((resolve, reject) => {
+      // get the data
+	  let tomorrowData = data.filter(function(tomorrowIn) {
+    return tomorrowIn.fullDate == tomorrow; });
+   // console.log(tomorrowData);
+      if (tomorrowData) {
+        // return the data
+        resolve(tomorrowData);
+      } else {
+        // return an error
+        reject(`Tomorrow object not found `);
+      }
+    });
+  }
+  //
+ // getting holiday data
+ async getHolidayData() {
+    return new Promise((resolve, reject) => {
+      // get the data
+	  let holidayData = data.filter(function(holidayIn) {
+    return holidayIn.aspRules == "Suspended"; });
+   // console.log(holidayData);
+//
+      if (holidayData) {
+        // return the data
+        resolve(holidayData);
+      } else {
+        // return an error
+        reject(`Holiday object not found `);
+      }
+    });
+  }
+  // add below
+  // add above
+}
+module.exports = Controller;
---
+++ index.js
@@ -0,0 +1,139 @@
+//index.js
+const http = require("http");
+const AppData = require("./controller");
+
+const PORT = process.env.PORT || 5005;
+
+const server = http.createServer(async (req, res) => {
+        // / : GET 
+    if (req.url === "/" && req.method === "GET") {
+        // set the status code, and content-type
+           res.writeHead(200, { "Content-Type": "application/json",
+           "Access-Control-Allow-Origin": "*" });
+           res.end(JSON.stringify({ message: "App is active! 🚀" }));
+       }
+    // /api/v1 : GET
+    else if (req.url === "/v1/list" && req.method === "GET") {
+        // get the data.
+        const allData = await new AppData().getAllData();
+        // set the status code, and content-type
+        res.writeHead(200, { "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*" });
+        // send the data
+        res.end(JSON.stringify(allData));
+    }
+    //
+
+	    //
+      // /api/v1/today : GET
+      else if (req.url === "/v1/today" &&
+      req.method === "GET") {
+             // get today data.
+            const todayData = await new AppData().getTodayData();
+            // set the status code, and content-type
+            res.writeHead(200, { "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"  });
+            // send the data
+            res.end(JSON.stringify(todayData));
+        }
+        //
+        // /api/v1/yesterday : GET
+        else if (req.url === "/v1/yesterday" &&
+      req.method === "GET") {
+             // get yesterday data.
+            const yesterdayData = await new AppData().getYesterdayData();
+            // set the status code, and content-type
+            res.writeHead(200, { "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"  });
+            // send the data
+            res.end(JSON.stringify(yesterdayData));
+        }
+        //
+        // /api/v1/tomorrow : GET
+        else if (req.url === "/v1/tomorrow" &&
+      req.method === "GET") {
+             // get tomorrow data.
+            const tomorrowData = await new AppData().getTomorrowData();
+            // set the status code, and content-type
+            res.writeHead(200, { "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"  });
+            // send the data
+            res.end(JSON.stringify(tomorrowData));
+        }
+        //
+    // /api/v1/holiday : GET
+    else if (req.url === "/v1/holiday" &&
+  req.method === "GET") {
+         // get holiday data.
+        const holidayData = await new AppData().getHolidayData();
+        // set the status code, and content-type
+        res.writeHead(200, { "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"  });
+        // send the data
+        res.end(JSON.stringify(holidayData));
+    }
+    //
+    //
+    // /v1 : GET 7 days Data
+    else if (req.url === "/v1/7days" && req.method === "GET") {
+         // get legal Holiday.
+        const dataNext7days = await new AppData().get7Data();
+        // set the status code, and content-type
+        res.writeHead(200, { "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"  });
+        // send the data
+        res.end(JSON.stringify(dataNext7days));
+    }
+    //
+    // /v1 : GET Year Data
+    else if (req.url === "/v1/year" && req.method === "GET") {
+        // get year data.
+       const dataFullYear = await new AppData().getYearData();
+       // set the status code, and content-type
+       res.writeHead(200, { "Content-Type": "application/json",
+       "Access-Control-Allow-Origin": "*"  });
+       // send the data
+       res.end(JSON.stringify(dataFullYear));
+   }
+   //
+
+ // /v1/date/:MMDDYY : GET
+ else if (req.url.match(/\/v1\/date\/([0-9]+)/) &&
+ req.method === "GET") {
+       try {
+           // get date 10272022 from url
+           const X = req.url.split("/")[3];
+           // get a single data
+           const singleData = await new AppData().getSingleData(X);
+           // set the status code and content-type
+           res.writeHead(200, { "Content-Type": "application/json",
+           "Access-Control-Allow-Origin": "*"  });
+           // send the data
+           res.end(JSON.stringify(singleData));
+       } catch (error) {
+           // set the status code and content-type
+           res.writeHead(404, { "Content-Type": "application/json",
+           "Access-Control-Allow-Origin": "*"  });
+           // send the error
+           res.end(JSON.stringify({ message: error }));
+       }
+   }
+// Add above
+
+    // No route present
+    else {
+        res.writeHead(404, { "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"  });
+        res.end(JSON.stringify({ message: "Route not found 💣" }));
+    }
+});
+
+
+server.listen(PORT, () => {
+    console.log(`server started on port: ${PORT}`);
+});
+
+// I will add credit or inspiration later.
+// Avoid a single error from crashing the server in production.
+process.on("uncaughtException", (...args) => console.error(args));
+process.on("unhandledRejection", (...args) => console.error(args));
