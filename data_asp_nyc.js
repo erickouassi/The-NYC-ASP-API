f737227ee1647b87fbf2cc3233586b6bc9d45868
Eric Kouassi <39168930+erickouassi@users.noreply.github.com>
2025-01-21 22:23:21 -0500

    Update data_asp_nyc.js @ change years

--- data_asp_nyc.js
+++ data_asp_nyc.js
@@ -1,7 +1,7 @@
 // For active year
 
-const array1 = require("./data_asp_nyc_2023");
-const array2 = require("./data_asp_nyc_2024");
+const array1 = require("./data_asp_nyc_2024");
+const array2 = require("./data_asp_nyc_2025");
 
 const newArray = array1.concat(array2);
 
